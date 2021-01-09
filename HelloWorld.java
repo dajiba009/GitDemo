2aefa2c484db576914037a3ed75038c8badbfc5a
dgb <new@qq.com>
2021-01-09 18:41:18 +0800

    第三条分支的提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("1");
-		System.out.println("HelloWorld");
-		System.out.println("2");
-		System.out.println("HelloWorld");
+		System.out.println("这是第三个分支");
+		
 	}
 }
