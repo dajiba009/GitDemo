e1861ca4bfa5871f9a2baa799997d7ce3ec03878
dgb <new@qq.com>
2021-01-14 11:48:40 +0800

    fix branch3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("1");
-		System.out.println("第二分支");
-		System.out.println("2");
-		System.out.println("1");
-		System.out.println("第二分支");
-		System.out.println("2");
+		System.out.println("第三分支");
 	}
 }
